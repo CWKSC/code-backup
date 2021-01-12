6eab36889289887498683043bb7a57eae3b49c4d
CWKSC <CWKSC.person@gmail.com>
2021-01-12 18:28:46 +0800

    Create main.cpp

---
+++ C++/ReadWriteFile/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+string readFile(const string &filename) {
+	ifstream file(filename);
+	string result;
+	file >> result;
+	file.close();
+	return result;
+}
+
+void writeFile(const string& filename, const string& content) {
+	ofstream file(filename);
+	file << content;
+	file.close();
+}
+
+int main() {
+	writeFile("file.txt", "Hello World!");
+	cout << readFile("file.txt");
+	return 0;
+}
